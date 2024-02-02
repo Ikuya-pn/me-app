2bcc5e4080bb5ae95740b7ae1f620d9df1b25007
L1ssas <peachang397@gmail.com>
2024-02-02 20:39:29 +0900

    first commit

---
+++ src/me-app/database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('admins')->insert([
+            'name' => 'admin1',
+            'email' => 'test1@test.com',
+            'password' => Hash::make('password123'),
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+    }
+}
